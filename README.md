3908b6278e7f281c3b3dd93f939afcaa1a32b925
AhmedSamy16 <ahmed.samy1640@gmail.com>
2024-02-20 02:40:27 +0200

    Project completed

---
+++ README.md
@@ -0,0 +1,3 @@
+# 02.Search-filter-project-React
+This is project 2/50 of my React projects series. it's an app that allows users to enter search keys and it filters the user list from the hardcode list based on the entered key.
+[Live Demo of the project click here](https://search-filter-project-react.netlify.app/)
---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import JSONDATA from './MOCK_DATA.json'
+  export default {
+    name: 'search-filter',
+    data() {
+      return {
+        users: JSONDATA,
+        query: ''
+      }
+    },
+    computed: {
+      filteredUsers() {
+        return this.users.filter(u => u.first_name.toLowerCase().includes(this.query.toLowerCase()))
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="app">
+    <input type="text" placeholder="search..." v-model="query" />
+    <div v-for="user in filteredUsers">
+      <p>{{ user.first_name }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+  .app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  input {
+    margin: 15px 0;
+    width: 280px;
+    height: 40px;
+    padding-left: 10px;
+    font-size: 20px;
+  }
+
+  p {
+    margin: 10px;
+    font-size: 18px;
+  }
+</style>
